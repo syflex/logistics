f16e54e6779ae52cb34b2232c95de302d31bf0fc
Simon Moses Onazi <syflex360@gmail.com>
2024-03-19 15:26:58 +0100

    Update client.ts

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'redis'
-const url = 'redis://default:2&Y>~F-h+_:Avc}@redis-10321.c10.us-east-1-4.ec2.cloud.redislabs.com:10321'
+const url = ''
 const redis = createClient({
     url: url
 })
