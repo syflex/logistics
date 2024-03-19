37d2a5d1d6718445d883529d0200637a5949af4a
Simon Moses Onazi <syflex360@gmail.com>
2024-03-19 16:32:04 +0100

    Delete src/exelTask.ts

--- src/exelTask.ts
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-export const handler = async (event) => {
-  if (event.httpMethod !== 'GET') {
-    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
-  }
-}
